bf0d9c07dc74aacbe8a21a0036abca7d492e3571
Ammar Al-Ghifary <ammaralghifary@gmail.com>
2019-09-30 09:06:37 +0700

    Register Java

--- app/src/main/java/com/blogspot/yourfavoritekaisar/ems/Forum/RegisterActivity.java
+++ app/src/main/java/com/blogspot/yourfavoritekaisar/ems/Forum/RegisterActivity.java
@@ -1,11 +1,18 @@
 package com.blogspot.yourfavoritekaisar.ems.Forum;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blogspot.yourfavoritekaisar.ems.R;
+import com.firebase.client.Firebase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,5 +32,71 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         login = findViewById(R.id.login);
 
+        Firebase.setAndroidContext(this);
+
+        login.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        });
+
+        registerButton.setOnClickListener(v -> {
+            user = username.getText().toString();
+            pass = password.getText().toString();
+
+            if (user.equals("")) {
+                username.setError("can't be blank");
+            } else if (pass.equals("")) {
+                password.setError("can't be blank");
+            } else if (!user.matches("[A-Za-z0-9]+")) {
+                username.setError("only alphabet or number allowed");
+            } else if (user.length() < 5) {
+                username.setError("at least 5 characters long");
+            } else if (pass.length() < 5) {
+                password.setError("at least 5 characters long");
+            } else {
+                final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
+                pd.setMessage("Loading...");
+                pd.show();
+
+                String url = "https://chatapp-60323.firebaseio.com/users.json";
+
+                StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Firebase reference = new Firebase("https://chatapp-60323.firebaseio.com/users");
+
+                        if (s.equals("null")) {
+                            reference.child(user).child("password").setValue(pass);
+                            Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                        } else {
+                            try {
+                                JSONObject obj = new JSONObject(s);
+
+                                if (!obj.has(user)) {
+                                    reference.child(user).child("password").setValue(pass);
+                                    Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "username already exists", Toast.LENGTH_LONG).show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        pd.dismiss();
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        System.out.println("" + volleyError);
+                        pd.dismiss();
+                    }
+                });
+
+                RequestQueue rQueue = Volley.newRequestQueue(Register.this);
+                rQueue.add(request);
+            }
+        });
     }
 }
