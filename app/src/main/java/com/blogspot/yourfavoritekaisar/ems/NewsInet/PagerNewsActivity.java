6fab1bfdf2948d770cc1bcd8a493ac7aea66a73d
Ziyat Akhsani <43949424+ziyataa@users.noreply.github.com>
2019-10-01 11:35:23 +0700

    Update PagerNewsActivity.java

--- app/src/main/java/com/blogspot/yourfavoritekaisar/ems/NewsInet/PagerNewsActivity.java
+++ app/src/main/java/com/blogspot/yourfavoritekaisar/ems/NewsInet/PagerNewsActivity.java
@@ -22,6 +22,6 @@ public class PagerNewsActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(viewPager);
     }
-
+// minjem bentar maru
 
 }
